7ecdcd150a89004cc3aa71a20cdce31d12b9de53
Sammy guttman <sguttman@itpmsoftware.com>
2024-02-20 15:10:38 -0500

    fix: error null body

--- src/controllers/meli.py
+++ src/controllers/meli.py
@@ -8,22 +8,31 @@ router = APIRouter()
 @router.get("/meli/notification")
 async def read_root(request: Request):
     body = await request.body()
+
+    if not body:
+        return {"error": "El cuerpo de la solicitud está vacío"}
+    
     json_body = json.loads(body.decode())
+    parametros = request.query_params
 
     with open("meli.json", "w") as f:
         json.dump(json_body, f)
 
-    return json_body
+    return {"body": json_body, "params": dict(parametros)}
 
 @router.get("/meli/credentials")
 async def read_root(request: Request):
     body = await request.body()
+    if not body:
+        return {"error": "El cuerpo de la solicitud está vacío"}
+    
     json_body = json.loads(body.decode())
+    parametros = request.query_params
 
     with open("credential.json", "w") as f:
         json.dump(json_body, f)
 
-    return json_body
+    return {"body": json_body, "params": dict(parametros)}
 
 
 __all__ = ["router"]

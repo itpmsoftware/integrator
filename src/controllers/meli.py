a7ba6ef3f372bc47ae5e3b61c9649c5cd166af1c
Sammy guttman <sguttman@itpmsoftware.com>
2024-02-21 09:57:19 -0500

    feat: create flow to auth on meli

--- src/controllers/meli.py
+++ src/controllers/meli.py
@@ -1,6 +1,10 @@
 import json
 from fastapi import APIRouter, Request
+from fastapi.responses import RedirectResponse
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 router = APIRouter()
 
@@ -21,18 +25,13 @@ async def read_root(request: Request):
     return {"body": json_body, "params": dict(parametros)}
 
 @router.get("/meli/credentials")
-async def read_root(request: Request):
-    body = await request.body()
-    if not body:
-        return {"error": "El cuerpo de la solicitud está vacío"}
-    
-    json_body = json.loads(body.decode())
-    parametros = request.query_params
+async def read_root(url_redirect: str):
 
-    with open("credential.json", "w") as f:
-        json.dump(json_body, f)
+    url_redirect = "{}?redirect={}".format(os.getenv("APP_URL"), url_redirect)
 
-    return {"body": json_body, "params": dict(parametros)}
+    link_access = """https://auth.mercadolibre.com.co/authorization?response_type=code&client_id={}&redirect_uri={}""".format(os.getenv("APP_MELI_ID"), url_redirect)
+
+    return RedirectResponse(url=link_access)
 
 
 __all__ = ["router"]

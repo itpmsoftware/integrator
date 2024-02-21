f2948a1bd26bedc6f61596d9a45f72e0d9456aa8
Sammy guttman <sguttman@itpmsoftware.com>
2024-02-21 11:21:08 -0500

    feat: add post method to recive notification

--- src/views/meli.py
+++ src/views/meli.py
@@ -1,8 +1,9 @@
-import json
+from typing import Optional
 from fastapi import APIRouter, Request
 from fastapi.responses import RedirectResponse
 from dotenv import load_dotenv
 import os
+import json
 
 
 from src.controllers.meli import get_token_meli
@@ -13,8 +14,35 @@ router = APIRouter()
 
 
 @router.get("/meli/notification")
-async def read_root(code: str):
-    return await get_token_meli(code)
+async def read_root(code: Optional[str] = None):
+    if(code):
+        return await get_token_meli(code)
+    else:
+        return None
+
+@router.post("/meli/notification")
+async def read_root(request: Request):
+    # Obtener el cuerpo de la solicitud
+    body = await request.body()
+    
+    # Verificar si el cuerpo de la solicitud está vacío
+    if not body:
+        return {"error": "El cuerpo de la solicitud está vacío"}
+    
+    # Decodificar el cuerpo de la solicitud
+    decoded_body = body.decode()
+    
+    # Cargar el cuerpo decodificado como JSON
+    json_body = json.loads(decoded_body)
+    
+    # Obtener todos los parámetros de la solicitud
+    parametros = request.query_params
+    
+    # Guardar json_body en un archivo .json
+    with open("datos.json", "w") as f:
+        json.dump(json_body, f)
+    
+    return {"body": json_body, "params": dict(parametros)}
 
 @router.get("/meli/credentials")
 async def read_root():
